5c970dfc97232b62a89a61db7c03a21b8885897b
Your Name <you@example.com>
2016-09-02 16:26:54 +0200

    nettoyage code + ok pour insertion img et supp + decommenter insertion du mp3

--- AUDIO/js_necessary_elem.js
+++ AUDIO/js_necessary_elem.js
@@ -5,23 +5,23 @@
 
 // tableau des données piste
 var PlayList = [
-{track: 1, artist: "Rihanna", album: "Anti", title:"Needed Me", src: "./mp3/neededme.mp3", imgsrc: "./img/neededme.jpg" }, // 0
-{track: 2, artist: "Young Money", /*album: "Rise Of An Empire",*/ title:"Senile", src: "./mp3/senile.mp3", imgsrc: "./img/senile.jpg" } , // 1
-{track: 3, artist: "Rihanna", album: "Good Girl Gone Bad", title:"Rehab", src: "./mp3/rehab.mp3", imgsrc: "./img/rehab.jpg" }, // 2
-{track: 4, artist: "Bjork", album: "Homogenic", title:"Joga", src: "./mp3/joga.mp3", imgsrc: "./img/joga.jpg" }, // 3
-{track: 5, artist: "Yemi Alade", album: "King of Queens", title:"Johnny", src: "./mp3/johnny.mp3", imgsrc: "./img/johnny.jpg" },// 4
+	{track: 1, artist: "Rihanna", album: "Anti", title:"Needed Me", src: "./mp3/neededme.mp3", imgsrc: "./img/neededme.jpg" }, // 0
+	{track: 2, artist: "Young Money", /*album: "Rise Of An Empire",*/ title:"Senile", src: "./mp3/senile.mp3", imgsrc: "./img/senile.jpg" } , // 1
+	{track: 3, artist: "Rihanna", album: "Good Girl Gone Bad", title:"Rehab", src: "./mp3/rehab.mp3", imgsrc: "./img/rehab.jpg" }, // 2
+	{track: 4, artist: "Bjork", album: "Homogenic", title:"Joga", src: "./mp3/joga.mp3", imgsrc: "./img/joga.jpg" }, // 3
+	{track: 5, artist: "Yemi Alade", album: "King of Queens", title:"Johnny", src: "./mp3/johnny.mp3", imgsrc: "./img/johnny.jpg" },// 4
 ];
 
 console.log("\nINSERER PISTE UN PAR UN DANS CHAQUE PLAYER");
 //parcourr la playlist et insérer petit à petit
 var tagAudio = document.getElementsByTagName('audio');
 var tagImg = document.getElementsByTagName('img');
-
 // écrire du texte dans les différents balbutiements
-  function setText(el,propriete) {
-    el.innerHTML = propriete;
+  function setText(el,text) {
+    el.innerHTML = text;
   }
-// PISTES DE MA PLAYLST ==
+
+// PISTES DE MA PLAYLST == et suppression de l'index
 for (var p = 0; p < PlayList.length, p < tagAudio.length, p < tagImg.length; p++) {
 	// parourir le tableau de piste
 	var pisteObj = PlayList[p]; // objet complet
@@ -29,64 +29,34 @@ for (var p = 0; p < PlayList.length, p < tagAudio.length, p < tagImg.length; p++
 	var image = PlayList[p].imgsrc;
 	var hOp = PlayList[p].hasOwnProperty('album'); // FALSE pour SENILE vérifie l'existence d'une propriété
 	console.log("var piste => numero ", p, piste, "\npiste Objet =>",pisteObj, hOp);
+	
+
 	/// remplacer la piste
 	tagImg[p].setAttribute('src', image);
-	//removedImg = pisteObj.splice(image, 1);
+	delete PlayList[p].imgsrc;
+	console.log("TAG image",pisteObj);
 
 	tagAudio[p].setAttribute('src', piste);
-	//removedAudio = PlayList[p].splice(piste, 1);
+	//delete PlayList[p].src;
+	//console.log("TAG audio",pisteObj);
+
 	//console.log("piste supprimée: ", PlayList.splice(0,1, piste));
 	console.log("piste ", p,"----", piste, image ,"----------------- Loop ", p+1);
-	getElementsByTagName('li').value;
+	document.getElementsByTagName('li');
 };
-
-
-
-//parcourir la playlist et insérer petit à petit
-for (var r = 0 ; r < tagAudio.length ; r++){
-	var replace = PlayList[r].src;
-// SUPPRIMER du tableauP "PlayList" la 'piste'
-console.log(replace);
-tagAudio[r].setAttribute('src', replace);
-console.log(PlayList.splice(0,1, replace));
-console.log("piste ", r,"----", replace ,"------ ----------- Loop ", r+1);
-//replace.eventListner('onplay', ftOnPlay)
-};
-
 
 // PASSER à une autre video:
 // Quand la piste est finie (piste.ended)
 // je charge l'autre piste piste(i+1).load() 
-
-
-
 
 // RECUPERER UN CONTAINER-PLAYER PAR ID
 var containerPlayerId = document.getElementById('p-1');
 // RECUPERER UN CONTAINER-PLAYER PAR class
 var containerPlayerClass = document.getElementsByClassName('p-1')[0];
 
-
-
-/*
-// RECUPERER UNE CLASS DANS UN CONTAINER-PLAYER
-//Sinon, puisqu'il y a plusieurs éléments (getElementsByClassName), un Array est renvoyé :
-//il faut donc lui spécifier un indice entre crochet :
-//var haut_gauche = document.getElementsByClassName("haut_gauche")[0];
-var track = document.getElementsByClassName('track')[0];
-var title = document.getElementsByClassName('title')[0];
-var album = document.getElementsByClassName('album')[0];
-var artist = document.getElementsByClassName('artist')[0];
-var controlers = document.getElementsByClassName('controlers')[0].getAttribute('src');
-
-//Ensuite, il faut passer par la méthode setAttribute() pour créer un attribut
-//(dans ce cas, c'est l'attribut class).
-// haut_gauche.setAttribute('class', 'haut_gauche');
-// haut_gauche.setAttribute('className', 'haut_gauche'); // pour IE
-*/
-
 // recuperer tous les propriété du tableau d'objet
 console.log("\nPROPRIETE DU TABLEAU D'OBJET PISTE");
+//METHOD #1 avec le 'for in  '
 //var propTab = []
 // for(var prop in pisteObj){
 // 	console.log("propriété ==>", prop);
@@ -94,6 +64,7 @@ console.log("\nPROPRIETE DU TABLEAU D'OBJET PISTE");
 	
 	pisteOwnProp= Object.getOwnPropertyNames(pisteObj);
 	console.log(pisteOwnProp);
+	
 for (var cpt = 0; cpt < pisteOwnProp.length; cpt++) {
 	pisteObj =PlayList[cpt];
 	console.log(pisteObj);
